a380732e7979d7222a575b188b3aff9b6101c35f
ysicing <i@ysicing.me>
2019-12-16 21:28:11 +0800

    improvement: update k8s

---
+++ scripts/gitbook/deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl -X PATCH \
+  -H "content-type: application/strategic-merge-patch+json" \
+  -H "$1" \
+  -d '{"spec":{"template":{"spec":{"containers":[{"name":"ysicing","image":"$3"}]}}}}' \
+  "$2/apis/apps/v1/namespaces/default/deployments/web-ysicing"
