323cc9f7a3976571d14eb513abb6c2d620ed8459
ysicing <ysicing@users.noreply.github.com>
2019-12-16 14:02:48 +0000

    Automated deployment: Mon Dec 16 14:02:48 UTC 2019 6c3c9d20cdee737bf3262f20c70ebaae0c226992

--- scripts/gitbook/deploy.sh
+++ scripts/gitbook/deploy.sh
@@ -3,5 +3,5 @@
 curl -X PATCH \
   -H "content-type: application/strategic-merge-patch+json" \
   -H "Authorization:Bearer $1" \
-  -d '{"spec":{"template":{"spec":{"containers":[{"name":"ysicing","image":"'"${$3}"'"}]}}}}' \
+  -d '{"spec":{"template":{"spec":{"containers":[{"name":"ysicing","image":"'"${3}"'"}]}}}}' \
   "$2/apis/apps/v1/namespaces/default/deployments/web-ysicing"
