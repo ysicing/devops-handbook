a18f3518a2f577fdbc3c4612a4db28ffb402b4f2
ysicing <ysicing@users.noreply.github.com>
2019-12-21 06:39:06 +0000

    Automated deployment: Sat Dec 21 06:39:06 UTC 2019 91d47bf5d0fcfb7d1ca32325e63153144dcd802d

---
+++ hack/docker/install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun
+
+cat > /etc/docker/daemon.json <<EOF
+{
+  "registry-mirrors": ["https://dockerhub.azk8s.cn","https://reg-mirror.qiniu.com"],
+  "bip": "172.30.42.1/16",
+  "max-concurrent-downloads": 10,
+  "log-driver": "json-file",
+  "log-level": "warn",
+  "log-opts": {
+    "max-size": "20m",
+    "max-file": "2"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+systemctl enable docker
+systemctl daemon-reload
+systemctl start docker
+systemctl restart docker 
+
+docker info -f "{{json .ServerVersion }}"
+
+docker run --rm -v /usr/local/bin:/sysdir ysicing/tools tar zxf /pkg.tgz -C /sysdir
