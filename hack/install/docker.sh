afc047d175906f620713b878c909689dc2201ce4
ysicing <i@ysicing.me>
2021-09-02 22:54:56 +0800

    fix(docker): fix docker install
    fix docker install
    
    Signed-off-by: ysicing <i@ysicing.me>

--- hack/install/docker.sh
+++ hack/install/docker.sh
@@ -1,41 +1,43 @@
 #!/bin/bash
 
-get_distribution() {
-	lsb_dist=""
-	# Every system that we officially support has /etc/os-release
-	if [ -r /etc/os-release ]; then
-		lsb_dist="$(. /etc/os-release && echo "$ID")"
-	fi
-	# Returning an empty string here should be alright since the
-	# case statements don't act unless you provide an actual value
-	echo "$lsb_dist"
-}
+set -e
 
-lsb_dist=$( get_distribution )
-lsb_dist="$(echo "$lsb_dist" | tr '[:upper:]' '[:lower:]')"
+# get_distribution() {
+# 	lsb_dist=""
+# 	# Every system that we officially support has /etc/os-release
+# 	if [ -r /etc/os-release ]; then
+# 		lsb_dist="$(. /etc/os-release && echo "$ID")"
+# 	fi
+# 	# Returning an empty string here should be alright since the
+# 	# case statements don't act unless you provide an actual value
+# 	echo "$lsb_dist"
+# }
 
-case "$lsb_dist" in
-		debian|raspbian)
-			dist_version="$(sed 's/\/.*//' /etc/debian_version | sed 's/\..*//')"
-			case "$dist_version" in
-				10)
-					update-alternatives --set iptables /usr/sbin/iptables-legacy && echo 1 || echo 0
-          			update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy && echo 1 || echo 0 
-          			update-alternatives --set arptables /usr/sbin/arptables-legacy && echo 1 || echo 0
-          			update-alternatives --set ebtables /usr/sbin/ebtables-legacy && echo 1 || echo 0
-				;;
-			esac
-		;;
-		*)
-			echo "..."
-		;;
-esac
+# lsb_dist=$( get_distribution )
+# lsb_dist="$(echo "$lsb_dist" | tr '[:upper:]' '[:lower:]')"
 
-curl -fsSL https://get.docker.com | bash -s docker --mirror AzureChinaCloud
+# case "$lsb_dist" in
+# 		debian|raspbian)
+# 			dist_version="$(sed 's/\/.*//' /etc/debian_version | sed 's/\..*//')"
+# 			case "$dist_version" in
+# 				10)
+# 					update-alternatives --set iptables /usr/sbin/iptables-legacy && echo 1 || echo 0
+#           			update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy && echo 1 || echo 0 
+#           			update-alternatives --set arptables /usr/sbin/arptables-legacy && echo 1 || echo 0
+#           			update-alternatives --set ebtables /usr/sbin/ebtables-legacy && echo 1 || echo 0
+# 				;;
+# 			esac
+# 		;;
+# 		*)
+# 			echo "..."
+# 		;;
+# esac
+
+curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun
 
 cat > /etc/docker/daemon.json <<EOF
 {
-  "registry-mirrors": ["https://reg-mirror.qiniu.com","https://dyucrs4l.mirror.aliyuncs.com","https://dockerhub.azk8s.cn"],
+  "registry-mirrors": ["https://dyucrs4l.mirror.aliyuncs.com"],
   "bip": "169.254.0.1/24",
   "max-concurrent-downloads": 10,
   "log-driver": "json-file",
@@ -55,4 +57,4 @@ systemctl restart docker
 
 docker info -f "{{json .ServerVersion }}"
 
-docker run --rm -v /usr/local/bin:/sysdir ysicing/tools tar zxf /pkg.tgz -C /sysdir
+docker run --rm -v /usr/local/bin:/sysdir registry.cn-beijing.aliyuncs.com/k7scn/tools tar zxf /pkg.tgz -C /sysdir
