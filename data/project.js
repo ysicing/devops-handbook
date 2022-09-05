95430383a439f141a33bc228c23c0d2335e8f17d
ysicing <i@ysicing.me>
2022-09-05 23:19:46 +0800

    feat(update): update
    update
    
    Signed-off-by: ysicing <i@ysicing.me>

--- data/project.js
+++ data/project.js
@@ -2,7 +2,7 @@ exports.projects = [
   {
     title: 'ergo',
     description: '开源运维工具',
-    preview: 'https://img.kuizuo.cn/kz-admin.png',
+    preview: 'https://github.com/ysicing.png',
     website: 'https://github.com/ysicing/ergo',
     source: 'https://github.com/ysicing/ergo',
     tags: ['opensource', 'favorite', 'product'],
@@ -10,9 +10,17 @@ exports.projects = [
   {
     title: 'spot',
     description: '开通腾讯云竞价工具',
-    preview: 'https://img.kuizuo.cn/kz-admin.png',
+    preview: 'https://github.com/ysicing.png',
     website: 'https://github.com/ysicing/spot',
     source: 'https://github.com/ysicing/spot',
     tags: ['opensource', 'product'],
+  },
+  {
+    title: 'zentao-api',
+    description: '禅道go sdk',
+    preview: 'https://github.com/ysicing.png',
+    website: 'https://github.com/ysicing/go-zentao',
+    source: 'https://github.com/ysicing/go-zentao',
+    tags: ['opensource'],
   }
 ]
