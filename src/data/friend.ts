0f24b5f78e2c89c861f5702eafec91c797df408d
ysicing <i@ysicing.me>
2023-03-08 23:18:55 +0800

    feat(update): update
    update
    
    Signed-off-by: ysicing <i@ysicing.me>

--- src/data/friend.ts
+++ src/data/friend.ts
@@ -18,6 +18,24 @@ export const Friends: Friend[] = [
     website: 'https://umami.external.ysicing.net/share/LnFwnhur/%E5%8D%9A%E5%AE%A2',
     avatar: require('./avatar/umami.png'),
   },
+  {
+    title: 'Status-云监控',
+    description: 'Status',
+    website: 'https://status.ysicing.cloud',
+    avatar: require('./avatar/default.png'),
+  },
+  {
+    title: 'IP查询',
+    description: 'IP查询',
+    website: 'https://ip.ysicing.cloud',
+    avatar: require('./avatar/default.png'),
+  },
+  {
+    title: '云缘生镜像站',
+    description: '云缘生镜像站',
+    website: 'https://mirrors.ysicing.cloud',
+    avatar: require('./avatar/default.png'),
+  },
 ];
 
 export function sortFriend() {
