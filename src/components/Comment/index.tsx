6755df92b4831fb25e236db11c501d65ddf547ac
ysicing <i@ysicing.me>
2023-03-20 00:21:59 +0800

    fix(typo): fix typo
    fix typo
    
    Signed-off-by: ysicing <i@ysicing.me>

--- src/components/Comment/index.tsx
+++ src/components/Comment/index.tsx
@@ -1,27 +1,28 @@
-import React, { useEffect }  from 'react'
-import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
+import React, { FC, useEffect }  from 'react'
 import 'artalk/dist/Artalk.css'
+import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
+import BrowserOnly from '@docusaurus/BrowserOnly';
+import { useLocalPathname } from '@docusaurus/theme-common/internal';
 import Artalk from 'artalk'
 
-export default function Comment() {
+export default function Comment(): JSX.Element {
+  const pathname = useLocalPathname();
    useEffect(() => {
-    let artalk: any = null;
-    if (ExecutionEnvironment.canUseDOM) {
+      let artalk: any = null;
+      if (ExecutionEnvironment.canUseDOM) {
       artalk = Artalk.init({
       el: '#comment',
       pageKey:   `${location.pathname}`,
       pageTitle: `${document.title}`,
       server:    'https://artalk.ysicing.cloud/',
       site:      '缘生笔记',
-      // ...
     });
     }
-    return () => {
-      if (artalk) artalk.destroy();
-    }
-   })
+   }, []);
 
   return (
-    <> <div id="comment" className="artalk-comments" /></>
+     <BrowserOnly fallback={<div id="comment" className="artalk-comments" />}>
+      {() => <div id="comment" className="artalk-comments" />}
+     </BrowserOnly>
   )
 }
