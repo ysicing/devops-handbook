a8f8f1de9090f838277301ed534e00336ffed714
ysicing <i@ysicing.me>
2023-03-20 10:36:07 +0800

    fix(typo): fix typo
    fix typo
    
    Signed-off-by: ysicing <i@ysicing.me>

--- src/components/Comment/index.tsx
+++ src/components/Comment/index.tsx
@@ -1,8 +1,13 @@
 import React, { useEffect }  from 'react'
 import 'artalk/dist/Artalk.css'
 import BrowserOnly from '@docusaurus/BrowserOnly';
+import { useLocalPathname } from '@docusaurus/theme-common/internal';
 
 export default function Comment(): JSX.Element {
+  let pathname = useLocalPathname();
+  if (pathname.endsWith('/')) {
+    pathname = pathname.slice(0, -1);
+  }
   return (
     <BrowserOnly fallback={<div>Loading Comments...</div>}>
       {() => {
@@ -10,7 +15,7 @@ export default function Comment(): JSX.Element {
         useEffect(() => {
           Artalk.init({
             el: '#comment',
-            pageKey: `${window.location.pathname}`,
+            pageKey: pathname,
             pageTitle: `${window.document.title}`,
             server: 'https://artalk.ysicing.cloud/',
             site: '缘生笔记',
